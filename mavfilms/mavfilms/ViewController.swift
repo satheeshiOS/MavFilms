e22bc51a31ed6f1dd6c167e2fd09d60ced647c52
Satheesh K <javish.fri@gmail.com>
2020-12-30 23:36:38 +0530

    Film Model added

---
+++ mavfilms/mavfilms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mavfilms
+//
+//  Created by Satheesh Speed Mac on 30/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
